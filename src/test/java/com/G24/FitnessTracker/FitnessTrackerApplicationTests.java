d7471bbd864c8aae6d1ac0576060ed755fda0fe6
lucasarabi <lucaarab03@gmail.com>
2026-03-07 14:27:12 -0500

    First commit

---
+++ src/test/java/com/G24/FitnessTracker/FitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G24.FitnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
