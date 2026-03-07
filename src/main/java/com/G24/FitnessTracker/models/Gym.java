b157ca91373e6615c67d9446c7380e8c6b4afbfa
Belyndda <143353652+Belyndda@users.noreply.github.com>
2026-03-07 17:59:09 -0500

    Add Gym model with JPA annotations

---
+++ src/main/java/com/G24/FitnessTracker/models/Gym.java
@@ -0,0 +1,30 @@
+package com.G24.FitnessTracker.models;
+
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "gym")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gymId;
+
+    @Column(nullable = false)
+    private String gymName;
+
+    private String location;
+
+    @OneToMany(mappedBy = "gym", cascade = CascadeType.ALL)
+    private List<Workout> workouts;
+
+    @OneToMany(mappedBy = "gym", cascade = CascadeType.ALL)
+    private List<Equipment> equipmentList;
+}
